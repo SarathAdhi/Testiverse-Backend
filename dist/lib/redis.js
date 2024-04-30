23562a0bcf920f53a7a60d94decaf46175f177d2
SarathAdhi <s.sarathadhithya@gmail.com>
2024-04-30 09:11:01 +0530

    removed remote redis

--- dist/lib/redis.js
+++ dist/lib/redis.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const ioredis_1 = __importDefault(require("ioredis"));
-const client = new ioredis_1.default(process.env.REDIS_URL);
+// const client = new Redis(process.env.REDIS_URL!);
+const client = new ioredis_1.default();
 exports.default = client;
 //# sourceMappingURL=redis.js.map
--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,5 +1,6 @@
 import Redis from "ioredis";
 
-const client = new Redis(process.env.REDIS_URL!);
+// const client = new Redis(process.env.REDIS_URL!);
+const client = new Redis();
 
 export default client;
